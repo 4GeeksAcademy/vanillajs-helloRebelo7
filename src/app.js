b952a66d05861e3ec85e56413f42f730233b105d
Rebelo7 <143350138+Rebelo7@users.noreply.github.com>
2023-10-16 19:06:17 +0000

    add excuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+};
+
+let generateExcuse = () => {
+  let who = ["The pope", "My yoga teacher", "His dinosaur", "My clone"];
+  let action = ["ate", "lost", "crushed", "broke"];
+  let what = ["my computer", "the keys", "the car"];
+  let when = [
+    "before the christmas eve",
+    "when i was meditating ",
+    "when I wake up",
+    "during rushhour",
+    "while I was doing karate"
+  ];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndx] +
+    " " +
+    action[actionIndx] +
+    " " +
+    what[whatIndx] +
+    " " +
+    when[whenIndx]
+  );
 };
